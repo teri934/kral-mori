0eb3e5df25bdf37c0cecb75ff5b66e5ed32d9567
Teri934 <56218654+Teri934@users.noreply.github.com>
2020-03-08 21:17:44 +0100

    random wind generator

--- ZapoctovaHra/Assets/scripts/wind_generator.cs
+++ ZapoctovaHra/Assets/scripts/wind_generator.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class wind_generator : MonoBehaviour
 {
     public static Vector3 position;
+    System.Random rd;
+    bool start;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rd = new System.Random();
+        start = true;
+    }
+
+    IEnumerator GenerateWind()
+    {
+        start = false;
+        position = new Vector3(rd.Next(-5, 5), 0, rd.Next(-5, 5));
+        yield return new WaitForSeconds(10);
+        start = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        position = Vector3.back;
+        if (start == true)
+            StartCoroutine(GenerateWind());
     }
 }

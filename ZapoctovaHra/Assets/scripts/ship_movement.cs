3371360aaa968a8288a269cf047665925a9cc627
Teri934 <56218654+Teri934@users.noreply.github.com>
2020-03-12 16:23:49 +0100

    anchor_creation

--- ZapoctovaHra/Assets/scripts/ship_movement.cs
+++ ZapoctovaHra/Assets/scripts/ship_movement.cs
@@ -6,31 +6,43 @@ public class ship_movement : MonoBehaviour
 {
     private Rigidbody rb;
     float angle;
+    private bool anchor;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anchor = false;
     }
 
     // Update is called once per frame
 
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            //transform.Rotate(0.0f, -1.0f, 0.0f, Space.Self);
-            rb.AddTorque(0f, -0.5f, 0f);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            rb.AddTorque(0f, 0.5f, 0f);
+            anchor = !anchor;
+            Debug.Log(anchor);
         }
 
         angle = Vector3.Angle(wind_generator.position, transform.forward);
         Debug.Log(angle);
 
-        rb.AddForce(3f * transform.forward + transform.forward * 3f * Mathf.Cos(angle * (Mathf.PI / 180)), ForceMode.Force);
+        if (anchor == false)
+        {
+            rb.AddForce(3f * transform.forward + transform.forward * 3f * Mathf.Cos(angle * (Mathf.PI / 180)), ForceMode.Force);
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                //transform.Rotate(0.0f, -1.0f, 0.0f, Space.Self);
+                rb.AddTorque(0f, -0.5f, 0f);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.AddTorque(0f, 0.5f, 0f);
+            }
+        }
+
     }
     void Update()
     {

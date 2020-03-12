db62ab279f00c43e6b5d516c79608eabba865759
Teri934 <56218654+Teri934@users.noreply.github.com>
2020-03-12 17:25:23 +0100

    correction_of_movement

--- ZapoctovaHra/Assets/scripts/ship_movement.cs
+++ ZapoctovaHra/Assets/scripts/ship_movement.cs
@@ -19,40 +19,39 @@ public class ship_movement : MonoBehaviour
     private void FixedUpdate()
     {
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            anchor = !anchor;
-            Debug.Log(anchor);
-        }
-
-
-        angle = Vector3.Angle(wind_generator.position, transform.forward);
         Debug.Log(angle);
-
 
         if (anchor == false)
         {
-            rb.AddForce(3f * transform.forward + transform.forward * 3f * Mathf.Cos(angle * (Mathf.PI / 180)), ForceMode.Force);
-
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                //transform.Rotate(0.0f, -1.0f, 0.0f, Space.Self);
-                rb.AddTorque(0f, -0.5f, 0f);
-            }
+            rb.AddForce(17f * transform.forward + transform.forward * 17f * Mathf.Cos(angle * (Mathf.PI / 180)), ForceMode.Force);
 
             if (Input.GetKey(KeyCode.LeftArrow))
             {
-                rb.AddTorque(0f, 0.5f, 0f);
+                rb.AddTorque(0f, -7f, 0f);
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.AddTorque(0f, 7f, 0f);
             }
         }
 
+    }
 
-		
-        rb.AddForce(3f * transform.forward + transform.forward * 3f * Mathf.Cos(angle * (Mathf.PI / 180)), ForceMode.Force);
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "cube")
+        {
+            rb.AddForce(10f * - transform.forward, ForceMode.Impulse);
+        }
     }
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anchor = !anchor;
+        }
+
+        angle = Vector3.Angle(wind_generator.position, transform.forward);
     }
 }

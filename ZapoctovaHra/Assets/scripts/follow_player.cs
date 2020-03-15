d60eff7ed75dab414e6e23e332cf0db734174c23
karhu-git <61918049+karhu-git@users.noreply.github.com>
2020-03-15 18:18:48 +0100

    Modified water shader
    + sea plane following player (shader-based tiling)

---
+++ ZapoctovaHra/Assets/scripts/follow_player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow_player : MonoBehaviour
+{
+	public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(player.transform.position.x-transform.position.x,0,player.transform.position.z-transform.position.z);
+    }
+}

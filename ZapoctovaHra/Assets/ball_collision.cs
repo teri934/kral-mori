7d5d3913b708323084aa53332dc2c9993e31e770
Teri934 <56218654+Teri934@users.noreply.github.com>
2020-03-15 23:32:40 +0100

    ball_firing_particles

---
+++ ZapoctovaHra/Assets/ball_collision.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_collision : MonoBehaviour
+
+{
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    IEnumerator DestroyBall()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "ship")
+        {
+            gameObject.GetComponent<ParticleSystem>().Play();
+            Destroy(rb);
+            StartCoroutine(DestroyBall());
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < 1.3f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- ZapoctovaHra/Assets/scripts/ship_movement.cs
+++ ZapoctovaHra/Assets/scripts/ship_movement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class ship_movement : MonoBehaviour
 {
     private Rigidbody rb;
-    float angle;
+    private float angle;
     private bool anchor;
+    private bool shoot;
+    public GameObject ball;
+    public float ball_force = 1500f;
 	public float speed = 10f;
 	public float rot_speed = 7f;
     // Start is called before the first frame update
@@ -14,6 +17,7 @@ public class ship_movement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         anchor = false;
+        shoot = true;
     }
 
     // Update is called once per frame
@@ -35,20 +39,39 @@ public class ship_movement : MonoBehaviour
                 rb.AddTorque(0f, rot_speed, 0f);
             }
         }
-
     }
 
     private void OnCollisionEnter(Collision collision)
     {
 		rb.AddForce(10f * - transform.forward, ForceMode.Impulse);
     }
+
+    IEnumerator Shoot()
+    {
+        shoot = false;
+        for (int i = -2; i < 3; i++)
+        {
+            GameObject new_ball = Instantiate(ball, transform.position + transform.forward * i, Quaternion.identity);
+            new_ball.GetComponent<Rigidbody>().AddForce((transform.right + transform.up) * ball_force, ForceMode.Impulse);
+        }
+        yield return new WaitForSeconds(3);
+        shoot = true;
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
             anchor = !anchor;
         }
-
         angle = Vector3.Angle(wind_generator.position, transform.forward);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (shoot == true)
+            {
+                StartCoroutine(Shoot());
+            }
+        }
     }
 }
